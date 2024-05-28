192504fd4000b020e6ed49e24babb674f9a155c5
Martin Liska <martin.liska@hey.com>
2024-05-28 16:48:26 +0200

    Implement first part

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,96 @@
-use std::env;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::io;
+use std::io::BufRead;
 use std::process;
+use std::str::FromStr;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+use clap::command;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Extended expression
+    #[arg(short)]
+    extended: String,
+}
+
+#[derive(Debug)]
+enum RegexToken {
+    CharacterGroup(HashSet<char>),
+}
+
+#[derive(Debug)]
+struct RegEx {
+    tokens: Vec<RegexToken>,
+}
+
+impl FromStr for RegEx {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut tokens = Vec::new();
+        let mut chars: VecDeque<_> = s.chars().collect();
+
+        while !chars.is_empty() {
+            let c = chars.pop_front().unwrap();
+
+            match c {
+                _ => tokens.push(RegexToken::CharacterGroup(HashSet::from([c]))),
+            }
+        }
+
+        Ok(RegEx { tokens })
     }
 }
 
-// Usage: echo <input_text> | your_grep.sh -E <pattern>
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+impl RegEx {
+    fn search(&self, s: &str) -> bool {
+        let chars: Vec<_> = s.chars().collect();
 
-    if env::args().nth(1).unwrap() != "-E" {
-        println!("Expected first argument to be '-E'");
-        process::exit(1);
+        for i in 0..chars.len() {
+            if self.search_from(i, &chars[i..]) {
+                return true;
+            }
+        }
+
+        false
     }
 
-    let pattern = env::args().nth(2).unwrap();
-    let mut input_line = String::new();
+    fn search_from(&self, start: usize, mut s: &[char]) -> bool {
+        for token in self.tokens.iter() {
+            if s.is_empty() {
+                return false;
+            }
 
-    io::stdin().read_line(&mut input_line).unwrap();
+            match token {
+                RegexToken::CharacterGroup(group) => {
+                    if !group.contains(&s[0]) {
+                        return false;
+                    }
+                    s = &s[1..];
+                }
+            }
+        }
 
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+        true
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let regex = RegEx::from_str(&args.extended).unwrap();
+
+    let mut line = String::new();
+    let mut handle = std::io::stdin().lock();
+    handle.read_line(&mut line).unwrap();
+
+    // strip the trailing newline
+    line = line.strip_suffix("\n").unwrap().to_string();
+    let searched = regex.search(&line);
+
+    println!("regex: {regex:?}, line: {line}, searched: {searched}");
+
+    std::process::exit(!searched as i32);
 }
